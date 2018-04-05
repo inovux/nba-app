0e286a32b176475ea355f997fd08c8ec3af8a762
Stanley Thijssen <stanley.thijssen@energybox.com>
2018-04-06 00:27:46 +0800

    finished header for articles routw

--- src/components/Articles/News/Post/header.js
+++ src/components/Articles/News/Post/header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import TeamInfo from '../../Elements/teamInfo'
+import PostData from '../../Elements/postData'
 
 const header = props => {
+
+  const teamInfo = (team) => {
+    return team ? (
+      <TeamInfo team={team} />
+    ) : null;
+  }
+
+  const postData = (date, author) => (
+    <PostData data={{date, author}} />
+  )
+
   return (
     <div>
-      Header
+      {teamInfo(props.teamData)}
+      {postData(props.date, props.author)}
     </div>
   );
 }
