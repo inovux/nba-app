e3a5820c05c988bd1f8b37831d45c12895c06ae5
Stanley Thijssen <stanley.thijssen@energybox.com>
2018-04-06 10:50:18 +0800

    Added header to videolist

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -16,17 +16,17 @@ class NewsArticles extends Component {
   componentDidMount() {
 
     axios.get(`${URL}/articles?id=${this.props.match.params.id}`)
-      .then(response => {
-        let article = response.data[0];
+    .then(response => {
+      let article = response.data[0];
 
-        axios.get(`${URL}/teams?id=${article.team}`)
-          .then(response => {
-            this.setState({
-              article,
-              team: response.data
-            });
-          })
-      });
+      axios.get(`${URL}/teams?id=${article.team}`)
+        .then(response => {
+          this.setState({
+            article,
+            team: response.data
+          });
+        })
+    });
 
   }
 
